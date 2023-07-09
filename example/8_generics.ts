ac32c14d04c9420f84509ef77f18a4404b57a9f3
이승원 <skmb1230@naver.com>
2023-07-10 01:54:56 +0900

    타입 스크립트 제네릭 실전 예제 추가

--- example/8_generics.ts
+++ example/8_generics.ts
@@ -25,19 +25,36 @@ function logNumber(num: number) {
 // 마찬가지로 유지보수 및 가독성이 불편해질듯..
 function logUnion(text: string | number) {
   console.log(text);
+  //text.split string과 number의 공통점에관한 부분만 접근가능.
+  //split같은 경우 string에서만 사용 가능하기 때문에 에러가 난다.
   return text;
 }
 
-logText("a");
-logNumber(10);
+//const a = logText("a");
+// a.split < 이것도 에러가 난다 number가 들어 갈 수 있기때문.
+//logNumber(10);
 
 //제네릭<T>
-//호출할때 타입을 넘겨준다.
+//호출 할 시점에 타입을 넘겨준다.
 //그렇다면 인자값과 리턴값 타입도 겨주면서 동적으로 정해줄 수 있다.
-// function logText<T>(text: T): T {
-//   console.log(text);
-//   return text;
-// }
+function logGenerics<T>(text: T): T {
+  console.log(text);
+  return text;
+}
 
-// // 명시적으로 string으로 정해주면 무슨타입이 들어갈지 명확하게 알 수 있다.
-// logText<string>("할루");
+// 명시적으로 string으로 정해주면 무슨타입이 들어갈지 명확하게 알 수 있다.
+const str = logGenerics<string>("할루");
+// str.split 제네릭으로 타입을 정해주었기 때문에 split 사용 가능.
+
+// boolean값으로 지정 되어 있다.
+const flag = logGenerics<boolean>(true);
+
+// 인터페이스에 제네릭 선언
+// 호출과 동시에 타입을 선언하면 string, number, boolean... 유연하게 사용 가능하다.
+// selected가 공통으로 들어 갈경우 사용하면 유용하다.
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const obj: Dropdown<string> = { value: "test", selected: true };
---
+++ example/dropdown-generic.ts
@@ -0,0 +1,52 @@
+// 인터페이스 제네릭을 이용하여 value값을 동적으로 사용 가능하게 만든다.
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+//인터페이스 타입을 지정하고 string을 제네릭에 넣어준다.
+const emails: DropdownItem<string>[] = [
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
+];
+
+const numberOfProducts: DropdownItem<number>[] = [
+  { value: 1, selected: true },
+  { value: 2, selected: false },
+  { value: 3, selected: false },
+];
+
+// createDropdownItem <T>에 동적으로 타입을 할당 받고
+// DropdownItem<T>에 위 할당받은 타입을 넣어서
+// DropdownItem<string> | DropdownItem<number>를 대체 할 수 있다.
+function createDropdownItem<T>(item: DropdownItem<T>) {
+  const option = document.createElement("option");
+  if (typeof item.value === "string") {
+    option.value = item.value.toString();
+    option.innerText = item.value.toString();
+  }
+  option.selected = item.selected;
+  return option;
+}
+
+// NOTE: 이메일 드롭 다운 아이템 추가
+emails.forEach(function (email) {
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector("#email-dropdown");
+  selectTag!.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem<number>(product);
+});
